492779c1ef903d0fcc8ce2e160f6031b60bd369f
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-07-06 06:20:13 +1000

    Exercises 1.2-1.3

--- src/intro.ml
+++ src/intro.ml
@@ -5,6 +5,7 @@ let rec lookup env x =
   | [] -> None
   | (y, v) :: r -> if x = y then Some v else lookup r x
 
+(* Exercise 1.1 *)
 type expr =
   | CstI of int
   | Var of string
@@ -30,3 +31,59 @@ let rec eval e env =
        | "min" -> if e1' < e2' then e1' else e2'
        | _ -> failwith ("unknown primitive " ^ op))
   | If (e1, e2, e3) -> if eval e1 env <> 0 then eval e2 env else eval e3 env
+
+(* Exercise 1.2.i *)
+type aexpr =
+  | CstI of int
+  | Var of string
+  | Add of aexpr * aexpr
+  | Mul of aexpr * aexpr
+  | Sub of aexpr * aexpr
+
+(* Exercise 1.2.ii *)
+(* v - (w + z) *)
+let e1 = Sub (Var "v", Add (Var "w", Var "z"))
+(* 2 * (v - w + z) *)
+let e2 = Mul (CstI 2, e1)
+(* x + y + z + w *)
+let e3 = Add (Add (Add (Var "x", Var "y"), Var "z"), Var "w")
+
+(* Exercise 1.2.iii, Exercise 1.3 *)
+let rec fmt' e n =
+  let m, s =
+    match e with
+    | CstI i -> 3, string_of_int i
+    | Var x -> 3, x
+    | Mul (e1, e2) -> 2, fmt' e1 2 ^ " * " ^ fmt' e2 3
+    | Add (e1, e2) -> 1, fmt' e1 1 ^ " + " ^ fmt' e2 2
+    | Sub (e1, e2) -> 1, fmt' e1 1 ^ " - " ^ fmt' e2 2
+  in if m < n then "(" ^ s ^ ")" else s
+
+let fmt e = fmt' e (-1)
+
+(* Exercise 1.2.iv *)
+let rec simplify e =
+  match e with
+  | CstI _ | Var _ -> e
+  | Mul (e1, e2)  ->
+      let e1' = simplify e1 in
+      let e2' = simplify e2 in
+      (match e1', e2' with
+       | (CstI 1, _) -> e2'
+       | (_, CstI 1) -> e1'
+       | (CstI 0, _) -> CstI 0
+       | (_, CstI 0) -> CstI 0
+       | _ -> e)
+  | Add (e1, e2) ->
+      let e1' = simplify e1 in
+      let e2' = simplify e2 in
+      (match (e1', e2') with
+       | (CstI 0, _) -> e2'
+       | (_, CstI 0) -> e1'
+       | _ -> e)
+  | Sub (e1, e2) ->
+      let e1' = simplify e1 in
+      let e2' = simplify e2 in
+      (match e1', e2' with
+       | (_, CstI 0) -> e1'
+       | _ -> if e1' = e2' then CstI 0 else e)
--- test/intro_test.ml
+++ test/intro_test.ml
@@ -51,6 +51,65 @@ let test_intro =
           (eval (If (CstI 5, CstI 1, CstI 2)) []);
         assert_equal ~msg:"test_eval_if_zero" 2
           (eval (If (CstI 0, CstI 1, CstI 2)) [])
+    );
+
+    "test_1.2.ii" >:: (
+      fun _ ->
+        assert_equal ~printer:(Printf.sprintf "%s") ~msg:"test_fmt_e1"
+          "v - (w + z)" (fmt e1);
+        assert_equal ~printer:(Printf.sprintf "%s") ~msg:"test_fmt_e2"
+          "2 * (v - (w + z))" (fmt e2);
+        assert_equal ~printer:(Printf.sprintf "%s") ~msg:"test_fmt_e3"
+          "x + y + z + w" (fmt e3)
+    );
+
+    "test_1.2.iv" >:: (
+      fun _ ->
+        assert_equal ~msg:"test_simplify_add_zerol" (Var "a")
+          (simplify (Add (CstI 0, Var "a")));
+        assert_equal ~msg:"test_simplify_add_zeror" (Var "a")
+          (simplify (Add (Var "a", CstI 0)));
+        assert_equal ~msg:"test_simplify_sub_zero" (Var "a")
+          (simplify (Sub (Var "a", CstI 0)));
+        assert_equal ~msg:"test_simplify_mul_onel" (Var "a")
+          (simplify (Mul (CstI 1, Var "a")));
+        assert_equal ~msg:"test_simplify_mul_oner" (Var "a")
+          (simplify (Mul (Var "a", CstI 1)));
+        assert_equal ~msg:"test_simplify_mul_zerol" (CstI 0)
+          (simplify (Mul (CstI 0, Var "a")));
+        assert_equal ~msg:"test_simplify_mul_zeror" (CstI 0)
+          (simplify (Mul (Var "a", CstI 0)));
+        assert_equal ~msg:"test_simplify_sub_eq_int" (CstI 0)
+          (simplify (Sub (CstI 1, CstI 1)));
+        assert_equal ~msg:"test_simplify_sub_neq_int"
+          (Sub (CstI 2, CstI 1)) (simplify (Sub (CstI 2, CstI 1)));
+        assert_equal ~msg:"test_simplify_sub_eq_var" (CstI 0)
+          (simplify (Sub (Var "a", Var "a")));
+        assert_equal ~msg:"test_simplify_sub_neq_var" (Sub (Var "a", Var "b"))
+          (simplify (Sub (Var "a", Var "b")));
+        assert_equal ~msg:"test_simplify_compound" (Var "x")
+          (simplify (Mul ((Add (CstI 0, CstI 1)), (Add (Var "x", CstI 0)))))
+    );
+
+
+    "test_1.3" >:: (
+      fun ctxt ->
+        assert_equal
+          ~printer:(Printf.sprintf "%s")
+          ~msg:"test_fmt_mul_sub" "(a - b) * c"
+          (fmt (Mul (Sub (Var "a", Var "b"), Var "c")));
+        assert_equal
+          ~printer:(Printf.sprintf "%s")
+          ~msg:"test_fmt_sub_mul" "a * b - c"
+          (fmt (Sub (Mul (Var "a", Var "b"), Var "c")));
+        assert_equal
+          ~printer:(Printf.sprintf "%s")
+          ~msg:"test_fmt_sub_subl" "a - b - c"
+          (fmt (Sub (Sub (Var "a", Var "b"), Var "c")));
+        assert_equal
+          ~printer:(Printf.sprintf "%s")
+          ~msg:"test_fmt_sub_subr" "a - (b - c)"
+          (fmt (Sub (Var "a", Sub(Var "b", Var "c"))))
     )
   ]
 
