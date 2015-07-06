a400311e44e2d7432d931143adc81939449dcfe0
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2015-07-06 14:33:53 +1000

    Fixed indentation. Couldn't figured out a better structure for simplify.

--- src/intro.ml
+++ src/intro.ml
@@ -16,20 +16,20 @@ let rec eval e env =
   match e with
   | CstI i -> i
   | Var x ->
-      (match lookup env x with
-       | None -> failwith (x ^ " not found")
-       | Some x -> x)
+    (match lookup env x with
+     | None -> failwith (x ^ " not found")
+     | Some x -> x)
   | Prim (op, e1, e2) ->
-      let e1' = eval e1 env in
-      let e2' = eval e2 env in
-      (match op with
-       | "+" -> e1' + e2'
-       | "*" -> e1' * e2'
-       | "-" -> e1' * e2'
-       | "==" -> if e1' = e2' then 1 else 0
-       | "max" -> if e1' > e2' then e1' else e2'
-       | "min" -> if e1' < e2' then e1' else e2'
-       | _ -> failwith ("unknown primitive " ^ op))
+    let e1' = eval e1 env in
+    let e2' = eval e2 env in
+    (match op with
+     | "+" -> e1' + e2'
+     | "*" -> e1' * e2'
+     | "-" -> e1' * e2'
+     | "==" -> if e1' = e2' then 1 else 0
+     | "max" -> if e1' > e2' then e1' else e2'
+     | "min" -> if e1' < e2' then e1' else e2'
+     | _ -> failwith ("unknown primitive " ^ op))
   | If (e1, e2, e3) -> if eval e1 env <> 0 then eval e2 env else eval e3 env
 
 (* Exercise 1.2.i *)
@@ -49,6 +49,7 @@ let e2 = Mul (CstI 2, e1)
 let e3 = Add (Add (Add (Var "x", Var "y"), Var "z"), Var "w")
 
 (* Exercise 1.2.iii, Exercise 1.3 *)
+(* Top-level for tracing. *)
 let rec fmt' e n =
   let m, s =
     match e with
@@ -66,24 +67,24 @@ let rec simplify e =
   match e with
   | CstI _ | Var _ -> e
   | Mul (e1, e2)  ->
-      let e1' = simplify e1 in
-      let e2' = simplify e2 in
-      (match e1', e2' with
-       | (CstI 1, _) -> e2'
-       | (_, CstI 1) -> e1'
-       | (CstI 0, _) -> CstI 0
-       | (_, CstI 0) -> CstI 0
-       | _ -> e)
+    let e1' = simplify e1 in
+    let e2' = simplify e2 in
+    (match e1', e2' with
+     | (CstI 1, _) -> e2'
+     | (_, CstI 1) -> e1'
+     | (CstI 0, _) -> CstI 0
+     | (_, CstI 0) -> CstI 0
+     | _ -> e)
   | Add (e1, e2) ->
-      let e1' = simplify e1 in
-      let e2' = simplify e2 in
-      (match (e1', e2') with
-       | (CstI 0, _) -> e2'
-       | (_, CstI 0) -> e1'
-       | _ -> e)
+    let e1' = simplify e1 in
+    let e2' = simplify e2 in
+    (match (e1', e2') with
+     | (CstI 0, _) -> e2'
+     | (_, CstI 0) -> e1'
+     | _ -> e)
   | Sub (e1, e2) ->
-      let e1' = simplify e1 in
-      let e2' = simplify e2 in
-      (match e1', e2' with
-       | (_, CstI 0) -> e1'
-       | _ -> if e1' = e2' then CstI 0 else e)
+    let e1' = simplify e1 in
+    let e2' = simplify e2 in
+    (match e1', e2' with
+     | (_, CstI 0) -> e1'
+     | _ -> if e1' = e2' then CstI 0 else e)
